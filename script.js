3daf0d386264570f91b4330037055ede0be27ba3
shellby.miller <shellby.miller@gmail.com>
2022-11-27 10:56:07 -0500

    added container and function to display current weather data of the searched city

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+//Global Variables
+var currentWeatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchedInputEl = document.querySelector("#searched-city");
+
 //Variable to store API key
 var APIKey = "61575cadc8adff6b8ca0fdec73b15a2d";
 
@@ -18,7 +23,39 @@ fetch(queryCurrentWeatherURL)
     });
 });
 
-var displayCurrentWeather 
+//Function for displaying current weather data for searched city inline at top of Weather Dashboard
+var displayCurrentWeather =function(weather,searchedCity){
+    currentWeatherContainerEl.textContent="";
+    citySearchedInputEl.textContent=searchedCity;
+
+//Format date element and append to searched city
+var currentDate = document.createElement("SPAN");
+currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+citySearchedInputEl.appendChild(currentDate);
+
+//Create image element and append to searched city
+var weatherIcon = document.createElement("img");
+weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+citySearchedInputEl.appendChild(weatherIcon);
+
+//Create span element to hold Temperature data
+var temperatureEl = document.createElement("SPAN");
+temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+
+//Create a span element to hold Humidity data
+var humidityEl = document.createElement("SPAN");
+humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+
+//Create a span element to hold Windspeed data
+var windSpeedEl = document.createElement("SPAN");
+windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+
+//Append span elements holding data to container for searched city's current weather
+currentWeatherContainerEl.appendChild(temperatureEl);
+currentWeatherContainerEl.appendChild(humidityEl);
+currentWeatherContainerEl.appendChild(windSpeedEl);
+
+};
 
 //API Call - Geocoder
 
