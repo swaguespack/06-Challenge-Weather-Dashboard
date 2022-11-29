cdb828509871adc7b00c7073ec67ff06bc82eb2a
shellby.miller <shellby.miller@gmail.com>
2022-11-28 19:42:52 -0500

    edits to search history button class list in JS

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ var searchHistory = function(searchHistory){
 
     searchHistoryEl = document.createElement("button");
     searchHistoryEl.textContent = searchHistory;
-    searchHistoryEl.classList = "d-flex w-100 btn-light border rounded p-2 mb-3 mt-3"
+    searchHistoryEl.classList = "d-flex w-100 justify-content-center border rounded p-2 mb-3 mt-3"
     searchHistoryEl.setAttribute("data-city", searchHistory)
     searchHistoryEl.setAttribute("type","submit");
 
